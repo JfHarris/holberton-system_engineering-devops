47866fbb4a7a3f202e829441fa44a8b372cfb47a
jfharris <jfharris1983@gmail.com>
2022-03-24 17:43:01 -0500

    Edit

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
 """for a given employee ID, returns info about his/her TODO list progress"""
-import requests
-from sys import argv
+def get_todo():
+    import requests
+    from sys import argv
+    id = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(id)).json()
+
+    task_list = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(id)).json()
+
+    completedTask = []
+
+    for task in task_list:
+        if task.get("completed") is True:
+            completedTask.append(task.get("title"))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user.get('name'), len(completedTask), len(task_list)))
+    for task in completedTask:
+        print("\t {}".format(task))
+
 
 if __name__ == '__main__':
-    try:
-        employ_id = int(argv[1])
-    except ValueError:
-        exit()
-
-    api_url = 'https://jsonplaceholder.typicode.com'
-    user_uri = '{api}/users/{id}'.format(api=api_url, id=employ_id)
-    todo_uri = '{user_uri}/todos'.format(user_uri=user_uri)
-
-    res = requests.get(user_uri).json()
-
-    employ = res.get("name")
-
-    res = requests.get(todo_uri).json()
-
-    totes = len(res)
-
-    no_task = sum([elem["completed"] is False for elem in res])
-
-    comp = totes - no_task
-
-    str = "Employee {name} is done with tasks({comp}/{totes}):"
-    print(str.format(name=employ, comp=comp, totes=totes))
-
-    for elem in res:
-        if elem.get('completed') is True:
-            print('\t', elem.get('title'))
+    get_todo()
